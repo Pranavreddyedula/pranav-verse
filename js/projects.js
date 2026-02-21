272c57de92733b6d88b3aad6652f734e79a9769f
Sai Pranavreddy <edulapranav@gmail.com>
2026-02-21 22:20:19 +0530

    Refactor project loading and update imports

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,5 @@
-import { db, collection, getDocs } from "./firebase.js";
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const container = document.getElementById("projects-container");
 
@@ -11,14 +12,23 @@ async function loadProjects() {
 
   querySnapshot.forEach((doc) => {
     const data = doc.data();
+    const id = doc.id;   // ⭐ IMPORTANT
 
     const card = `
       <div class="project-card">
-        <img src="${data.image}" />
+        <img src="${data.image}" style="width:100%;border-radius:10px" />
+
         <h3>${data.title}</h3>
-        <p>${data.description}</p>
+        <p>${data.description || ""}</p>
+
+        <a href="view-project.html?id=${id}">
+          <button>Open Project</button>
+        </a>
+
+        <br><br>
+
         <a href="${data.github}" target="_blank">GitHub</a>
-        <a href="${data.live}" target="_blank">Live</a>
+        <a href="${data.live}" target="_blank">Live Demo</a>
       </div>
     `;
 
